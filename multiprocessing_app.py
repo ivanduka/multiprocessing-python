aea9197a162b8289aa00c20cce8cb99375235934
Ivan Duka <ivan.duka.pm@gmail.com>
2019-12-13 15:32:07 -0700

    Final

--- multiprocessing_app.py
+++ multiprocessing_app.py
@@ -1,47 +1,94 @@
-# from multiprocessing import Pool
-# import os
-# import time
-# from subprocess import run, TimeoutExpired
-#
-#
-# def do_work(args):
-#     start = time.time()
-#     try:
-#         run(args, timeout=12)
-#     except TimeoutExpired:
-#         print(f"Timeout of process: {args}")
-#         return None, args
-#     item_duration = round(time.time() - start)
-#     return item_duration, args
-#
-#
-# if __name__ == "__main__":
-#     cwd = os.path.dirname(os.path.realpath(__file__))
-#     exePath = os.path.join(cwd, "go.exe")
-#
-#     work_items = list(map(lambda x: [exePath, f"{x}"], list(range(24))))
-#
-#     start_time = time.time()
-#
-#     with Pool() as pool:
-#         results = pool.map(do_work, work_items)
-#
-#     duration = time.time() - start_time
-#
-#     done_items = {}
-#     for result in results:
-#         if result[0]:
-#             done_items[result[1][1]] = result[0]
-#
-#     timeout_items = set()
-#     for result in results:
-#         if not result[0]:
-#             timeout_items.add(result[1][1])
-#
-#     print(done_items)
-#     print(timeout_items)
-#
-#     print(done_items["1"])
-#     print("5" in done_items)
-#
-#     print(f"Python: done {len(work_items)} in {round(duration)} seconds.")
+import os
+from multiprocessing import Pool
+import time
+from subprocess import run, TimeoutExpired, CalledProcessError, DEVNULL
+from glob import glob
+
+
+# Function runs a single unit of work (processing one PDF)
+def do_work(args):
+    debugging = False  # change to True/False to see/not see the pdf2html output/errors
+    std = None if debugging else DEVNULL
+    timeout = 20 * 60  # timeout in seconds before terminating a subprocess, default is 1200s (20 min)
+
+    pdf_name = args[2].split("\\")[-1]
+    pdf_index = args[0]
+    arguments = args[1:]
+    start = time.time()
+
+    try:
+        run(arguments, timeout=timeout, stderr=std, stdout=std)
+    except (TimeoutExpired, CalledProcessError):
+        print(f"{pdf_index}: Timed out {pdf_name} in {format(timeout / 60, '.1f')} minutes")
+        return False, pdf_name
+
+    item_duration = round(time.time() - start)
+    print(f"{pdf_index} finished {pdf_name} in {format(item_duration / 60, '.1f')} minutes.")
+    return True, pdf_name
+
+
+def parse_results(outputs):
+    success_items = set()
+    timeout_items = set()
+    for result in outputs:
+        success, name = result
+        if success:
+            success_items.add(name)
+        else:
+            timeout_items.add(name)
+    return success_items, timeout_items
+
+
+# Function converts full paths to PDFs into a command line arguments for pdfToHtml executable
+def create_arguments(inputs):
+    args = []
+    for j in range(len(inputs)):
+        current_file = pdf_files[j]
+        current_file_index = f"{j + 1}/{len(pdf_files)}"
+        arg = [current_file_index, pdf_to_html_exe, current_file, "--dest-dir", out_directory]
+        args.append(arg)
+    return args
+
+
+if __name__ == "__main__":
+    # Marking the start time of the script
+    start_time = time.time()
+
+    # Path to pdfToHtml executable file
+    pdf_to_html_exe = r"F:/Environmental Baseline Data/Version 2/Code/EXE Files/pdf2htmlEX-win32-0.14.6-upx-with-poppler-data/pdf2htmlEX.exe"
+
+    # Path to the folder with PDF files
+    pdf_files = sorted(glob(r"F:\Environmental Baseline Data\Version 3\Data\PDF\*.pdf"))[:24]
+
+    # Path to the output folder where you need HTML files to be stored
+    out_directory = r"c:\Users\T1Ivan\Desktop\PDF\out"
+
+    # Converting full file paths to arguments for pdfToHtml
+    work_items = create_arguments(pdf_files)
+
+    # Header information block
+    print("\n###########################")
+    print(f"Starting {len(pdf_files)} PDFs")
+    print("###########################\n")
+
+    # Running all the work in multiprocessing mode
+    with Pool() as pool:
+        results = pool.map(do_work, work_items)
+
+    # === [For reference only] the sequential processing way ===
+    # results = []
+    # for work_item in work_items:
+    #     results.append(do_work(work_item))
+
+    # Sorting the results into two lists with multiprocessing: successfully processed and time outed PDFs
+    success_pdfs, timeout_pdfs = parse_results(results)
+
+    # Calculating the duration of the script run
+    duration = time.time() - start_time
+
+    # Footer information block
+    print("\n###########################")
+    print(f"Done {len(success_pdfs)} PDFs in {round(duration)} seconds.")
+    print(f"{len(success_pdfs)} PDFs are done: {list(success_pdfs)}")
+    print(f"{len(timeout_pdfs)} PDFs timed out: {list(timeout_pdfs)}")
+    print("###########################")
