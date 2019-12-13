86814cef4c02127c1b533392b16377262d65d735
Ivan Duka <ivan.duka.pm@gmail.com>
2019-12-12 22:32:07 -0700

    Fixed Windows path

--- PDF_to_HTML_Conversion.py
+++ PDF_to_HTML_Conversion.py
@@ -1,47 +1,84 @@
+import json
 from multiprocessing import Pool
-import os
 import time
-from subprocess import run, TimeoutExpired
+from subprocess import run, TimeoutExpired, CalledProcessError, DEVNULL
+from glob import glob
 
 
 def do_work(args):
+    debugging = False  # change to True/False to see/not see the pdf2html output/errors
+    std = None if debugging else DEVNULL
+    timeout = 5  # in seconds
+
+    pdf_name = args[2].split("\\")[-1]
+    pdf_index = args[0]
+    arguments = args[1:]
     start = time.time()
+
     try:
-        run(args, timeout=12)
-    except TimeoutExpired:
-        print(f"Timeout of process: {args}")
-        return None, args
+        run(arguments, timeout=timeout, stderr=std, stdout=std)
+    except (TimeoutExpired, CalledProcessError):
+        print(f"{pdf_index}: Timed out {pdf_name} in {format(timeout / 60, '.1f')} minutes")
+        return False, pdf_name
+
     item_duration = round(time.time() - start)
-    return item_duration, args
+    print(f"{pdf_index} finished {pdf_name} in {format(item_duration / 60, '.1f')} minutes.")
+    return True, pdf_name
+
+
+def parse_results(outputs):
+    success_items = set()
+    timeout_items = set()
+    for result in outputs:
+        success, name = result
+        if success:
+            success_items.add(name)
+        else:
+            timeout_items.add(name)
+    return success_items, timeout_items
+
+
+def get_unprocessed_files(current_files):
+    try:
+        with open("success.json", "r+") as json_file:
+            data = json.load(json_file)
+            done_set = set(data)
+            all_set = set(current_files)
+            remaining_set = all_set - done_set
+            return list(remaining_set)
+    except IOError:
+        return current_files
 
 
 if __name__ == "__main__":
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    exePath = os.path.join(cwd, "go.exe")
+    pdfToHtmlExe = r"C:\Users\T1Ivan\Desktop\GitHub\multiprocessing-python\pdf2htmlEX-win32-0.14.6-upx-with-poppler-data\pdf2htmlEX.exe"
+    pdf_files = sorted(glob(r"c:\Users\T1Ivan\Desktop\PDF\*.pdf"))[:]
+    # pdf_files = glob(r"F:\Environmental Baseline Data\Version 3\Data\PDF\*.pdf")
+    out_directory = r"c:\Users\T1Ivan\Desktop\PDF\out"
 
-    work_items = list(map(lambda x: [exePath, f"{x}"], list(range(24))))
+    pdf_path = pdf_files[0].split
+    unprocessed_pdf_files = get_unprocessed_files(pdf_files)
+
+    work_items = []
+    for i in range(len(pdf_files)):
+        pdf_file = pdf_files[i]
+        command_line_argument = [f"{i}/{len(pdf_files)}", pdfToHtmlExe, pdf_file, "--dest-dir", out_directory]
+        work_items.append(command_line_argument)
 
     start_time = time.time()
+    print("\n###########################")
+    print(f"Starting {len(work_items)} PDFs")
+    print("###########################\n")
 
     with Pool() as pool:
         results = pool.map(do_work, work_items)
 
     duration = time.time() - start_time
 
-    done_items = {}
-    for result in results:
-        if result[0]:
-            done_items[result[1][1]] = result[0]
+    success_pdfs, timeout_pdfs = parse_results(results)
 
-    timeout_items = set()
-    for result in results:
-        if not result[0]:
-            timeout_items.add(result[1][1])
-
-    print(done_items)
-    print(timeout_items)
-
-    print(done_items["1"])
-    print("5" in done_items)
-
-    print(f"Python: done {len(work_items)} in {round(duration)} seconds.")
+    print("\n###########################")
+    print(f"Done {len(success_pdfs)} of {len(work_items)} PDFs in {round(duration)} seconds.")
+    print(f"{len(success_pdfs)} PDFs are done: {success_pdfs}")
+    print(f"{len(timeout_pdfs)} PDFs timed out: {timeout_pdfs}")
+    print("###########################")
--- multiprocessing_app.py
+++ multiprocessing_app.py
@@ -1,47 +1,47 @@
-from multiprocessing import Pool
-import os
-import time
-from subprocess import run, TimeoutExpired
-
-
-def do_work(args):
-    start = time.time()
-    try:
-        run(args, timeout=12)
-    except TimeoutExpired:
-        print(f"Timeout of process: {args}")
-        return None, args
-    item_duration = round(time.time() - start)
-    return item_duration, args
-
-
-if __name__ == "__main__":
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    exePath = os.path.join(cwd, "go.exe")
-
-    work_items = list(map(lambda x: [exePath, f"{x}"], list(range(24))))
-
-    start_time = time.time()
-
-    with Pool() as pool:
-        results = pool.map(do_work, work_items)
-
-    duration = time.time() - start_time
-
-    done_items = {}
-    for result in results:
-        if result[0]:
-            done_items[result[1][1]] = result[0]
-
-    timeout_items = set()
-    for result in results:
-        if not result[0]:
-            timeout_items.add(result[1][1])
-
-    print(done_items)
-    print(timeout_items)
-
-    print(done_items["1"])
-    print("5" in done_items)
-
-    print(f"Python: done {len(work_items)} in {round(duration)} seconds.")
+# from multiprocessing import Pool
+# import os
+# import time
+# from subprocess import run, TimeoutExpired
+#
+#
+# def do_work(args):
+#     start = time.time()
+#     try:
+#         run(args, timeout=12)
+#     except TimeoutExpired:
+#         print(f"Timeout of process: {args}")
+#         return None, args
+#     item_duration = round(time.time() - start)
+#     return item_duration, args
+#
+#
+# if __name__ == "__main__":
+#     cwd = os.path.dirname(os.path.realpath(__file__))
+#     exePath = os.path.join(cwd, "go.exe")
+#
+#     work_items = list(map(lambda x: [exePath, f"{x}"], list(range(24))))
+#
+#     start_time = time.time()
+#
+#     with Pool() as pool:
+#         results = pool.map(do_work, work_items)
+#
+#     duration = time.time() - start_time
+#
+#     done_items = {}
+#     for result in results:
+#         if result[0]:
+#             done_items[result[1][1]] = result[0]
+#
+#     timeout_items = set()
+#     for result in results:
+#         if not result[0]:
+#             timeout_items.add(result[1][1])
+#
+#     print(done_items)
+#     print(timeout_items)
+#
+#     print(done_items["1"])
+#     print("5" in done_items)
+#
+#     print(f"Python: done {len(work_items)} in {round(duration)} seconds.")
