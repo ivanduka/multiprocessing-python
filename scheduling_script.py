8b1217fc7ace0551e32919bc7566632b533c3606
Ivan Duka <ivan.duka.pm@gmail.com>
2020-01-28 11:23:13 -0700

    Saving and emailing works

--- scheduling_script.py
+++ scheduling_script.py
@@ -9,14 +9,16 @@ import pandas as pd
 from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from pathlib import Path
+from datetime import datetime
 
 load_dotenv()
 
 
 def get_something_from_f_drive():
-    folder = r"F:\Environmental Baseline Data\text_from_all_files_in_REGDOCS\txt"
+    folder = Path(r"\\luxor\data\Branch\Environmental Baseline Data\text_from_all_files_in_REGDOCS\txt")
     pdf_files = glob(f"{folder}/*.json")
-    print(f"Number of files in {folder}: {len(pdf_files)}")
+    print(f'Number of files in "{folder}": {len(pdf_files)}')
     print()
 
 
@@ -31,12 +33,14 @@ def get_something_from_db():
 
 def simple_printing():
     print("Simple printing of Python executable file location: " + sys.executable)
+    # C:\Users\T1Ivan\AppData\Local\Continuum\anaconda3\python.exe
+    # "C:\Users\T1Ivan\Desktop\GitHub\multiprocessing-python\scheduling_script.py"
     print()
 
 
 def running_external_app():
     timeout = 1 * 60  # in seconds
-    arguments = ["ping", "8.8.8.8", "-n", "1"]
+    arguments = ["ping", "8.8.8.8"]
     result = {}
 
     try:
@@ -50,18 +54,26 @@ def running_external_app():
 
 def send_email(text):
     message = Mail(
-        from_email='ivan.duka@prime_minister_office.ca',
-        to_emails=["ivan.duka@cer-rec.gc.ca", 'ivan.duka.pm@gmail.com'],
+        from_email='justin@pmoffice.ca',
+        to_emails=["ivan.duka@cer-rec.gc.ca", "Margaret.Skwara@cer-rec.gc.ca", "Janna.Rodioukova@cer-rec.gc.ca"],
         subject='Testing the automatically scheduled Python script ;)',
         plain_text_content=text)
     try:
         sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
         response = sg.send(message)
         print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        # print(response.body)
+        # print(response.headers)
     except Exception as e:
         print(str(e))
+
+
+def save_to_file(text):
+    file_name = datetime.today().strftime('%Y-%m-%d at %H.%M.%S') + ".txt"
+    file_path = folder.joinpath(file_name)
+    print(f"Saving to the file: '{file_path}'")
+    with open(file_path, 'w+') as the_file:
+        the_file.write(text)
 
 
 def main():
@@ -72,9 +84,17 @@ def main():
 
 
 if __name__ == "__main__":
+    folder = Path(r"\\luxor\data\Branch\Environmental Baseline Data\text_from_all_files_in_REGDOCS")
     sys.stderr = sys.stdout
     with io.StringIO() as out_buf, contextlib.redirect_stdout(out_buf):
+        print("Hi! This is an automatically generated email that is scheduled and is run periodically.")
+        print("It demonstrates access to drive F, querying a DB, running a terminal command, etc.")
+        print("It also captures every 'print' statement, output from externally ran apps, etc.,")
+        print(f"and saves it to a file in '{folder}' as well as sends it via email")
+        print()
         main()
         output = out_buf.getvalue()
-    send_email(output)
     print(output)
+    print()
+    send_email(output)
+    save_to_file(output)
