683fb909bc4caff0bd2244864af68656a31cd9ad
Ivan Duka <ivan.duka.pm@gmail.com>
2020-01-29 14:57:58 -0700

    Camelot multiprocessing

---
+++ concurrent_camelot.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+import time
+from glob import glob
+from pathlib import Path
+import camelot
+from multiprocessing import Pool
+
+source_pdf_dir = Path(r'F:\Environmental Baseline Data\01 ESAs as PDF\ESA Documents')
+results_dir = Path(r'F:\Environmental Baseline Data\text_from_all_files_in_REGDOCS\PDFs\results\\')
+
+
+def process_table(pdf):
+    try:
+        tables = camelot.read_pdf(str(pdf), pages='all', flag_size=True, copy_text=['v'], line_scale=40)
+        print(f'Process {os.getpid()} Processing File Name:{pdf}\nTotal Tables found:{len(tables)}')
+        with open(results_dir.joinpath(f"{pdf.stem}.pkl"), 'wb') as f:
+            pickle.dump(tables, f)
+            print(f'Process {os.getpid()} Pickle file created for: {pdf}')
+        with open(results_dir.joinpath(f"{pdf.stem}.pkl"), 'rb') as g:
+            pickle.load(g)
+            print(f'Process {os.getpid()} Pickle file loaded: {pdf}')
+        return True
+    except Exception as e:
+        return e
+
+
+def process_handler():
+    pdf_files = sorted(glob(f"{source_pdf_dir}\\*.pdf"))[:]
+    pdf_paths = []
+    for pdf_file in pdf_files:
+        pdf_paths.append(Path(pdf_file))
+
+    print(f"Working on {len(pdf_paths)}")
+
+    start_time = time.time()
+
+    # Sequential process
+    results = []
+    # for pdf in pdf_paths:
+    #     results.append(process_table(pdf))
+
+    # Multiprocessing
+    with Pool() as pool:
+        results = pool.map(process_table, pdf_paths)
+
+    print(results)
+
+    duration = round(time.time() - start_time)
+    print(f'Whole Process completed in {duration} second(s)')
+
+
+if __name__ == '__main__':
+    process_handler()
