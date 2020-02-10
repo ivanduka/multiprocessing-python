fac255233b679b7b5d3dd18c360b87604478f49f
Ivan Duka <ivan.duka.pm@gmail.com>
2020-02-10 15:15:39 -0700

    species

--- species.py
+++ species.py
@@ -1,14 +1,53 @@
 import requests
+import json
+import pandas as pd
+
+
+def get_data_provider():
+    items_per_request = 1000  # cannot go over 1000
+    skip = 0
+    done = False
+
+    def get_data():
+        nonlocal skip
+        nonlocal done
+        if done:
+            return False
+
+        url = "https://ecprccsarsrch.search.windows.net/indexes/speblobidxen/docs/search?api-version=2017-11-11&="
+
+        payload = "{\n    \"filter\": \"\",\n    \"queryType\": \"full\",\n    \"searchMode\": \"all\"," \
+                  "\n    \"search\": \"*\",\n    \"orderby\": \"commonNameSort asc,taxonomySort asc\"," \
+                  "\n    \"count\": false,\n    \"top\": %s,\n    \"skip\": %s,\n    \"select\": \"id," \
+                  "legalStatusId,cosewicStatusId,legalCommonName,cosewicCommonName,cosewicScientificName," \
+                  "legalScientificName,legalPopulation,cosewicPopulation,taxonomyId,emergencyAssessment," \
+                  "cosewicLastAssessmentDate,imageFileName,legalDateListing,legalScheduleId,underConsiderationId," \
+                  "ranges\"\n}" % (items_per_request, skip)
+
+        headers = {'Content-Type': 'application/json', 'api-key': '3A1E8E87503C069448999238ABD05EE9'}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        decoded = json.loads(response.text)
+        result = decoded['value']
+        if len(result) < items_per_request:
+            done = True
+        skip += items_per_request
+        return result
+
+    return get_data
+
 
 if __name__ == "__main__":
-    url = "https://ecprccsarsrch.search.windows.net/indexes/docblobidxen/docs/search?api-version=2017-11-11&="
+    dp = get_data_provider()
+    data = []
+    while True:
+        chunk = dp()
+        if not chunk:
+            break
+        data += chunk
 
-    payload = "{\n    \"filter\": \"\",\n    \"queryType\": \"full\",\n    \"searchMode\": \"all\",\n    \"search\": \"*\",\n    \"orderby\": \"documentTypeSort asc,sortDate desc,documentCreateDate asc,documentTitleSort asc\",\n    \"count\": false,\n    \"top\": 1000,\n    \"skip\": 0,\n    \"select\": \"id,consultationEndDate,consultationStartDate,consultationActivationStatusId,documentCreateDate,documentDescription,documentTitle,documentTypeId,species,attachments,contacts,links,finalOrDelayed\"\n}"
-    headers = {
-        'Content-Type': 'application/json',
-        'api-key': '3A1E8E87503C069448999238ABD05EE9'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text.encode('utf8'))
+    df = pd.DataFrame(data)
+    print("Headers:", df.columns.values)
+    print("Top rows:")
+    print(df.head())
+    print("Total rows:", len(df.index))
+    # df.to_csv("out.csv", encoding='utf-8')
