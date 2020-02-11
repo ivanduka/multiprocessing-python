d5a63497860d411d6e1534cfdebf6af611988ba0
Ivan Duka <ivan.duka.pm@gmail.com>
2020-02-10 17:57:24 -0700

    species

---
+++ get_the_total_size_of_regdocs.py
@@ -0,0 +1,175 @@
+import os
+from glob import glob
+from multiprocessing import Pool
+import time
+from sqlalchemy import create_engine
+import pandas as pd
+import requests
+from pathlib import Path
+from subprocess import run, TimeoutExpired, CalledProcessError
+import shutil
+import json
+from PyPDF2 import PdfFileReader
+
+# Path to results folder:
+text_files_folder = Path(r'C:\Users\T1Ivan\Desktop\tmp')
+
+
+def do_work(file_id):
+    start = time.time()
+
+    pdf_content = download_file(file_id)
+    if not pdf_content:
+        create_dummy_json(file_id)
+        return {file_id: {"result": "Download failed"}}
+
+    metadata = convert_pdf_to_json(file_id, pdf_content)
+    if not metadata:
+        return {file_id: {"result": "Processing failed"}}
+
+    item_duration = round(time.time() - start)
+    print("Processed", file_id, "in", item_duration, "seconds")
+
+    return {
+        file_id: {"result": "Success", "metadata": metadata}
+    }
+
+
+def create_dummy_json(file_id):
+    temp_file_path = text_files_folder.joinpath(f"{file_id}.json")
+    with open(temp_file_path, 'w') as document:
+        pass
+
+
+def download_file(file_id):
+    url = f'https://apps.cer-rec.gc.ca/REGDOCS/File/Download/{file_id}'
+    with requests.Session() as session:
+        r = session.get(url)
+        content_type = r.headers["Content-Type"]
+        if content_type == 'application/pdf':
+            print(f"Downloaded ID {file_id} (size {len(r.content)})")
+            return r.content
+        else:
+            print(f"Failed downloading ID {file_id}: {r.status_code} - {content_type}")
+            return None
+
+
+def convert_pdf_to_json(file_id, pdf):
+    timeout = 60 * 20  # in seconds
+    jar_file_path = Path(__file__).parent.joinpath("buildvu-html-trial.jar")
+
+    temp_file_path = text_files_folder.joinpath(f"{file_id}.pdf")
+    with open(temp_file_path, "x+b") as tf:
+        tf.write(pdf)
+        tf.flush()
+
+        try:
+            read_pdf = PdfFileReader(tf)
+            document_info = read_pdf.getDocumentInfo()
+            document_pages = read_pdf.getNumPages()
+            document_xmp_metadata = read_pdf.getXmpMetadata()
+        except Exception:
+            read_pdf = None
+            document_info = None
+            document_pages = None
+            document_xmp_metadata = None
+
+    document_size = os.path.getsize(temp_file_path)
+
+    args = ['java', "-DsocksProxyHost=socks.example.com", "-Xmx2000M", "-d64", '-jar', f"{jar_file_path}",
+            f'{temp_file_path}', f'{text_files_folder}', ]
+
+    base_dir = text_files_folder.joinpath(temp_file_path.stem)
+    source_json = base_dir.joinpath("search.json")
+    target_json = base_dir.parent.joinpath(str(file_id) + ".json")
+
+    try:
+        run(args, timeout=timeout)
+    except Exception:
+        print(f'Failed to process ID {file_id}')
+        if base_dir.exists():
+            shutil.rmtree(base_dir)
+        os.remove(temp_file_path)
+        return None
+
+    if source_json.exists():
+        os.replace(source_json, target_json)
+
+    if base_dir.exists():
+        shutil.rmtree(base_dir)
+
+    os.remove(temp_file_path)
+
+    return {
+        "document_info": document_info,
+        "document_pages": document_pages,
+        "document_xmp_metadata": document_xmp_metadata,
+        "document_size": document_size,
+    }
+
+
+def get_ids():
+    conn_str = 'mssql+pyodbc://psql21cap/CS_Prod?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server'
+    conn = create_engine(conn_str)
+    query = 'SELECT DataID from DTreeCore WHERE SubType = 144'
+    df = pd.read_sql(query, conn)
+    print(f"DB returned {df.shape[0]} IDs")
+    ids = df['DataID'].values.tolist()  # converting dataframe to list of ID's
+    return ids
+
+
+def remove_processed(all_ids):
+    pdf_files = glob(f"{text_files_folder}/*.json")
+
+    processed_ids = set([])
+    for pdf_file in pdf_files:
+        processed_ids.add(int(Path(pdf_file).stem))
+
+    new_ids = []
+    for next_id in all_ids:
+        if next_id not in processed_ids:
+            new_ids.append(next_id)
+
+    return new_ids
+
+
+def cleanup_folders_and_pdfs():
+    pdf_files = glob(f"{text_files_folder}/*.pdf")
+    folders = os.listdir(text_files_folder)
+
+    print(f"Cleaning up pdf files and folders")
+
+    for pdf in pdf_files:
+        os.remove(pdf)
+
+    for folder in folders:
+        full_path = text_files_folder.joinpath(folder)
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+
+
+if __name__ == "__main__":
+    cleanup_folders_and_pdfs()
+
+    ids_from_db = get_ids()[:]
+    ids_for_processing = remove_processed(ids_from_db)
+
+    print(f"Skipping {len(ids_from_db) - len(ids_for_processing)} items (already processed)")
+    print(f"Starting to work on {len(ids_for_processing)} items")
+
+    start_time = time.time()
+
+    # Running all the work in a multiprocessing mode
+    with Pool() as pool:
+        results = pool.map(do_work, ids_for_processing)
+
+    # Running all the work in a sequential mode
+    # results = []
+    # for single_id in ids_for_processing:
+    #     results.append(do_work(single_id))
+
+    # with open('report.json', 'w') as f:
+    #     json.dump(results, f, indent=2)
+
+    duration = round(time.time() - start_time)
+    print(f"Done in {duration} seconds")
--- species.py
+++ species.py
@@ -3,16 +3,15 @@ import json
 import pandas as pd
 
 
-def get_data_provider():
+def get_data():
+    result = []
     items_per_request = 1000  # cannot go over 1000
     skip = 0
     done = False
 
-    def get_data():
-        nonlocal skip
-        nonlocal done
+    while True:
         if done:
-            return False
+            return result
 
         url = "https://ecprccsarsrch.search.windows.net/indexes/speblobidxen/docs/search?api-version=2017-11-11&="
 
@@ -27,27 +26,87 @@ def get_data_provider():
         headers = {'Content-Type': 'application/json', 'api-key': '3A1E8E87503C069448999238ABD05EE9'}
         response = requests.request("POST", url, headers=headers, data=payload)
         decoded = json.loads(response.text)
-        result = decoded['value']
-        if len(result) < items_per_request:
+        received = decoded['value']
+        if len(received) < items_per_request:
             done = True
         skip += items_per_request
-        return result
-
-    return get_data
+        result += received
 
 
-if __name__ == "__main__":
-    dp = get_data_provider()
-    data = []
-    while True:
-        chunk = dp()
-        if not chunk:
-            break
-        data += chunk
+def get_data_dictionary():
+    r = requests.request("GET", "https://species-registry.canada.ca/sar-config.json")
+    data_dict = r.json()
+    return data_dict['species']["en"]["columns"]
 
-    df = pd.DataFrame(data)
-    print("Headers:", df.columns.values)
-    print("Top rows:")
-    print(df.head())
-    print("Total rows:", len(df.index))
-    # df.to_csv("out.csv", encoding='utf-8')
+
+def replace_cell(prev_value, options_dict):
+    to_string_for_uniformity = str(prev_value)
+    if to_string_for_uniformity in options_dict:
+        return options_dict[to_string_for_uniformity]
+    else:
+        return prev_value
+
+
+def remove_tags(prev_value):
+    return prev_value.replace("<i>", "").replace("</i>", "")
+
+
+# ['1', '11', '12', '13', '2', '3', '4', '5', '6', '7', '8', '9']
+def replace_ranges(prev_value, options_dict):
+    result = []
+    for numberString in prev_value:
+        num = str(numberString)
+        if num in options_dict:
+            result.append(options_dict[num])
+        else:
+            result.append(num)
+    return result
+
+
+def apply_data_dictionary(raw_df, columns):
+    for column in columns:
+        name = column["name"]
+        label = column["label"]
+        options = column["options"]
+        if name in raw_df:
+            raw_df = raw_df.rename(columns={name: label})
+
+            if label == "COSEWIC scientific name":
+                raw_df[label] = raw_df[label].apply(remove_tags)
+
+            if options:
+                options_dict = {}
+                for option in options:
+                    key = str(option["id"])
+                    value = str(option["label"])
+                    options_dict[key] = value
+
+                if label == "Range":
+                    raw_df[label] = raw_df[label].apply(replace_ranges, args=(options_dict,))
+                else:
+                    raw_df[label] = raw_df[label].apply(replace_cell, args=(options_dict,))
+    return raw_df
+
+
+data = get_data()
+df = pd.DataFrame(data)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.width', 9999)
+
+print("Headers:", df.columns.values)
+print()
+print("Top rows:")
+print()
+print(df.head(10))
+print()
+print("Total rows:", len(df.index))
+
+
+data_dictionary = get_data_dictionary()
+parsed_df = apply_data_dictionary(df, data_dictionary)
+
+print()
+print(parsed_df.head(10))
+
+# df.to_csv("out_.csv", encoding='utf-8')
